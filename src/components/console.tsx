e4c3cff04baa6a71ec539e399686b21c06e65b1b
Dongmoon Kim <ehdans04@gmail.com>
2024-01-07 17:30:15 +0100

    add: ascii arc

--- src/components/console.tsx
+++ src/components/console.tsx
@@ -2,7 +2,14 @@
 import { ThemeContext } from '@/context/ThemeContext';
 import { useTypingEffect } from '@/hooks/useTyping';
 import { useContext } from 'react';
+import { OsxWindowButtons } from './osxWindowButtons';
 
+const asciiDog = `
+  __      _
+o'')}____//
+ \`_/      )
+ (_(_/-(_/
+`;
 const Console = () => {
   const text = useTypingEffect();
   const { theme } = useContext(ThemeContext);
@@ -11,16 +18,17 @@ const Console = () => {
     <div
       className={`rounded-xl ${
         theme === 'light' ? 'bg-gray-100' : 'bg-gray-800'
-      }  w-80 md:w-96 h-80`}>
+      }  w-full sm:w-3/5 h-96`}>
       <div className="flex bg-gray-600 min-h-8 rounded-t-xl p-2 gap-2">
-        <div className="rounded-full bg-red-500 w-4"></div>
-        <div className="rounded-full bg-yellow-300 w-4"></div>
-        <div className="rounded-full bg-green-300 w-4"></div>
+        <OsxWindowButtons />
       </div>
-      <div className="rounded-b-xl p-5 min-h-60">
+      <div className="rounded-b-xl p-3 pb-1 min-h-60">
+        <div className="text-center mb-5">
+          <pre>{asciiDog}</pre>
+        </div>
         <div
           className={`relative m-auto ${
-            theme === 'light' ? 'text-black' : 'text-gray-200'
+            theme === 'light' ? 'text-gray-700' : 'text-gray-200'
           } text-sm text-left h-50 w-150`}>
           <span>$ {text}</span>
         </div>
