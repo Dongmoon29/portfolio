74e2007dee2d0ecd799b47f251eef4d0bdab673c
Dongmoon Kim <ehdans04@gmail.com>
2024-01-07 17:29:07 +0100

    delete: considering delete header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider>
           <ThemeWrapper>
-            <Header />
+            {/* <Header /> */}
             {children}
           </ThemeWrapper>
         </ThemeProvider>
